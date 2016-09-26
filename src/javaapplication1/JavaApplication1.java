b21fe41b0e983a6390f62713af6568d2ca5aecd4
andnaneadjana <andnaneadjana@172.29.126.112>
2016-09-26 10:10:01 -0400

    Ajout d'une ligne nous allons apprendre a utiliser git

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         System.out.println("Hello word");
+        System.out.println("Nous allons apprendre a utiliser git");
         
     }
     
